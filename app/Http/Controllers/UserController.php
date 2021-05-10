f164be6966d30801f077e96529d0e67c88f958ac
loremZyn14 <yasuaimo@gmail.com>
2021-05-10 13:26:27 +0800

    update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return ['users' => $users,
+        'token' => $users->first()->createToken('token_ni')->plainTextToken
+        ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+
+        $request->validate([
+            'name' => 'required'
+        ],[
+            'name.required' => 'Butangi na dong.'
+        ]);
+        $user->update([
+            'name' => $request->name
+        ]);
+
+        return $user;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,3 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/users', [UserController::class,'index']);
+Route::get('/users/{user}', [UserController::class,'show']);
+Route::put('/users/{user}', [UserController::class,'update']);
+Route::delete('/users/{user}', [UserController::class,'destroy']);
+
+// Route::resource('users', UserController::class)->except(['edit','edit']);
